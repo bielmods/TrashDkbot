c0f3ccbf38a5a23c730f02a2e0dcb6cc94479477
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-01-29 14:57:40 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,108 @@
+const help = (prefix) => {
+	return `ঔৣ͜͡TrashфDk'Ofcꪶ(BOT)comandos:
+
+Comando : *${prefix}sticker* ou *${prefix}stiker*
+desc : converter imagem / gif / vídeo em adesivo
+uso : responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda\n
+
+comando : *${prefix}sticker nobg* ou *${prefix}stiker nobg*
+desc : converter imagem em adesivo removendo o fundo
+uso : responder imagem ou enviar imagem com legenda\n
+
+comando : *${prefix}toimg*
+desc : converter sticker para imagem
+uso : cite um sticker\n
+
+comando : *${prefix}tsticker* ou *${prefix}tstiker*
+desc : converte texto em sticker
+uso : *${prefix}tsticker Texto aqui*\n
+
+> *Comandos de meme* <
+comando : *${prefix}meme*
+desc : Meme aleatório [Inglês]
+uso : apenas envie o comando\n
+
+comando : *${prefix}memeindo*
+desc : memes aleatórios [indonesio]
+uso : apenas envie o comando\n
+
+> *Outros comandos* <
+comando : *${prefix}gtts*
+desc : converte texto em áudio
+uso : *${prefix}gtts [cc] [text]*\nexample : *${prefix}gtts pt iae mano*\n
+
+comando : *${prefix}loli*
+desc : imagens de lolis aleatórias
+uso : apenas envie o comando\n
+
+comando : *${prefix}nsfwloli*
+desc : fotos de loli nfsw aleatórias
+uso : apenas envie o comando\n
+
+comando : *${prefix}url2img*
+desc : pega prints de sites
+uso : *${prefix}url2img [tipe] [url]*\n
+
+comando : *${prefix}simi*
+desc : suas mensagens serao respondidas pelo simi
+uso : *${prefix}simi (sua mensagem)*\n
+
+comando : *${prefix}ocr*
+desc : transforma o texto em pintura
+uso : mande imagem,ou marque imagem com legenda\n
+
+comando : *${prefix}wait*
+desc : procurar o anime com uma imagem [ que anime é este ]
+uso : responder imagem ou enviar imagem com legenda\n
+
+comando : *${prefix}setprefix*
+desc : recoloca o prefix
+uso : *${prefix}setprefix [text|optional]*\n exemplo : *${prefix}setprefix ?*
+note : Esse comando só pode ser usado pelo dono do bot 😎🤙\n
+
+> *Comandos de grupo* <
+comando : *${prefix}add*
+desc : Adiciona um membro ao grupo
+uso : *${prefix}add 55849xxxxx*\n
+note : só pode ser usado quando o bot se torna admin, e quem envia também é admin!\n
+
+comando : *${prefix}kick*
+desc : expulsa membros do grupo
+uso : *${prefix}kick @marcarmembro*\n
+note : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+
+comando : *${prefix}promote*
+desc : faz um membro comum se tornar adm😎🤙
+uso : *${prefix}promote @marcarmembro*\n
+note : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+
+comando : *${prefix}demote*
+desc : faz um adm virar membro comum 😔🤙
+uso : *${prefix}demote @marcarmembro*\n
+note : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+
+comando : *${prefix}linkgroup*
+desc : envia o link do grupo
+uso : apenas envie o comando
+note : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+
+comando : *${prefix}leave*
+desc : faz o bot sair do grupo
+uso : apenas envie o comando
+note : so pode ser usado por admins ou o dono do bot\n
+
+comando : *${prefix}tagall*
+desc : marca todos os membros e admins
+uso : apenas envie o comando
+note : Este comando so pode ser usado se você for admin\n
+
+comando : *${prefix}simih*
+desc : ativa o modo simih
+uso : *${prefix}simih 1* pra ativar o modo, e *${prefix}simih 0* pra desativar o modo simih
+note : Este comando so pode ser usado se você for admin do grupo\n`
+=============================================================
+Contate meu dono caso tenha alguma dúvida: wa.me/558494740630
+=============================================================
+}
+
+exports.help = help
