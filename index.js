9dd61a08e9aa1112189de60caebae3ed090ac5ab
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-01-30 16:58:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -666,9 +666,8 @@ async function starts() {
 					}
 					break
 case 'dono':
-if (args.length < 1) return reply(ঔ☬✞ঔ𝐓𝐫𝐚𝐬𝐡ф𝐃𝐤'𝐎𝐟𝐜ꪶ ✞☬ঔ😎🤙
-'Número:WA.me/558494740630 
-Youtube:https://youtube.com/channel/UCOqlFzRrB8IZUWUx1cwCeUw )
+if (args.length < 1) return reply('ঔ☬✞ঔ𝐓𝐫𝐚𝐬𝐡ф𝐃𝐤'𝐎𝐟𝐜ꪶ ✞☬ঔ
+Número do meu dono😎🤙:WA.me/558494740630 Etc\n 😎🤙Youtube do meu dono:https://youtube.com/channel/UCOqlFzRrB8IZUWUx1cwCeUw etc')
 break
 				default:
 					if (isGroup && isSimi && budy != undefined) {
