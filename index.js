fcd6d7780b17a8b589145d6522cb0ce60c07bceb
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-02-01 13:54:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -667,7 +667,13 @@ async function starts() {
 					break
                                 case 'dono':
 if (args.length < 1) return reply('TrashфDkofcꪶ Número do meu dono😎🤙:WA.me/558494740630 😎🤙Youtube do meu dono:https://youtube.com/channel/UCOqlFzRrB8IZUWUx1cwCeUw Número da cremosa do meu dono que ajudou a fazer o menu 😎🤙ঔৣ☬✞𝐓𝐫𝐚𝐬𝐡|𝐉𝐮𝐰 ✞☬ঔৣ: wa.me/558491093889')
-break
+                                        break
+                                case 'porno':
+					memein = await kagApi.memeindo()
+					buffer = await getBuffer(`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx3BgnL2qAHDTlfCPMAvdjuLGvOx402dSdhw&usqp=CAU`)
+					client.sendMessage(from, buffer, image, {quoted: mek, caption: 'Adm proibiu porno no gp🙄'})
+					break
+				
                                 	default:
 					if (isGroup && isSimi && budy != undefined) {
 						console.log(budy)
