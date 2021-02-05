1f8f87b08f8477a4c6f954ae4f189ea2842b7cea
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-02-05 01:13:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -330,7 +330,6 @@ async function starts() {
 					break
 				case 'loli':
                                         gatauda = body.slice(6)
-                                        if (!isUser) return reply(mess.only.daftarB)
                                         reply(mess.wait)
                                         anu = await fetchJson(`https://tobz-api.herokuapp.com/api/randomloli?apikey=BotWeA`, {method: 'get'})
                                         buffer = await getBuffer(anu.result)
