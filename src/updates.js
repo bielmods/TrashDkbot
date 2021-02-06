efe4e04f299abd4bf47e5b350167d977f3b546bb
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-02-06 12:37:17 -0300

    Add files via upload

---
+++ src/updates.js
@@ -0,0 +1,26 @@
+const updates = (prefix, pushname) => {
+ return `
+Novos comandos atualizados pelo meu dono 
+novos comandos :  {prefix}Ytmp4, {prefix}Setbotpp, {prefix}Blocklist, 
+
+
+{prefix}wame
+
+{prefix}image
+
+{prefix}loli(refeito funcionando)
+
+{prefix}grupo fechar
+
+{prefix}grupo abrir
+
+
+
+
+digite {prefix}help pra ver os comandos com mais detalhes
+ `
+
+
+}
+
+exports.updates = updates
