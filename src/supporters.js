d27fed3c3739ad047517d5d8b32670d590546f24
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-02-10 11:19:15 -0300

    Update supporters.js

--- src/supporters.js
+++ src/supporters.js
@@ -1,3 +1,6 @@
+const supporters = () => {
+    return `
+
 ❌✞ ʟʊɨֆ ✞❌
 𝑁𝐾 𝑊𝐴𝑅 𝛩𝐹 𝑃𝜟𝐼𝑵𝜩𝑳
 
@@ -7,3 +10,8 @@ Se vc quer se tornar um supporters,entre em contato com o meu dono,
 
 
 Aviso- pra se tornar supporters vc precisará rajar travas em pessoas q tentarem derrubar o bot.
+`
+
+}
+
+exports.supporters = supporters
